971dc33c0d82adaebeaf5637501a764e73393a15
Sergiy <sergiy.shevchuk@icloud.com>
2025-02-01 10:25:19 -0500

    init

---
+++ SUI-Template/SUI_TemplateApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SUI_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SUI-TemplateTests/SUI_TemplateTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import SUI_Template
+
+struct SUI_TemplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
